7a14393b79e2c9c0203dda59d200897b9bca9372
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-07 16:57:16 -0600

    feat(backend/Proveedores) : creacion del Dto base para los endpoints

---
+++ Backend/src/Meseta_Verde.Application/Common/DTOs/ProveedoresDtos/ProveedorDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meseta_Verde.Application.Common.DTOs.ProveedoresDtos
+{
+    public class ProveedorDto
+    {
+        public int IdProveedor {get;set;}
+        public int IdUsuario {get;set;}
+        public string NombreProveedor {get;set;}
+        public string NombreFinca {get;set;}
+        public string UbicacionGps {get;set;}
+        public string Biografia {get;set;}
+        public float CalificacionPromedio {get;set;}
+
+    }
+}
