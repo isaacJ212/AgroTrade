d5f59f2633f33ac78f05daf9bbf1a05fa14c930d
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-07 17:56:37 -0600

    feat(backend/Proveedores): Creacion de ProveedorController y creacion del endpoitn de GetAll de proveedores

---
+++ Backend/src/Meseta_Verde/Controllers/ProveedoresController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Meseta_Verde.Application.Common.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Meseta_Verde.Application.Common.DTOs.ProveedoresDtos;
+using Meseta_Verde.Application.Common;
+using Meseta_Verde.Application.Features.Proveedores.Queries;
+
+
+namespace Meseta_Verde.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProveedoresController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProveedoresController(IMediator mediator, IUnitOfWork unitOfWork)
+        {
+            _mediator = mediator;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<List<ProveedorDto>>>> Get()
+        {
+            var result = await _mediator.Send(new GetProveedoresQuery());
+            return Ok(result);
+        }
+        
+    }
+}
