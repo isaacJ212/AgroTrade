95d212539bbfdb12c65a900cc9f78335e74fc583
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-09 14:26:59 -0600

    feat(backend/InventarioProveedor): Agregar Interfaz IStorageService para desacoplar Logica de negocio a Supabase

---
+++ Backend/src/Meseta_Verde.Application/Common/Interface/IStorageService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meseta_Verde.Application.Common.Interface
+{
+    public interface IStorageService
+    {
+        Task<string> UploadFileAsync(IForm file, string buckectName, string fileName, CancellationToken ct);
+        Task DeleteFileAsync(string bucketName,string fileUrl, CancellationToken ct);
+    }
+}
