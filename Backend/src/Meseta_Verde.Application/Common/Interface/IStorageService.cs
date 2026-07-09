b67e033f17de2a4167579cfc66b38bf0c4693f4d
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-09 14:37:02 -0600

    feat(backend/InventarioProveedor): Agregarmos el Dto para luego crear el command y hacemos correccion en IStorageService

--- Backend/src/Meseta_Verde.Application/Common/Interface/IStorageService.cs
+++ Backend/src/Meseta_Verde.Application/Common/Interface/IStorageService.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Meseta_Verde.Application.Common.Interface
 {
     public interface IStorageService
     {
-        Task<string> UploadFileAsync(IForm file, string buckectName, string fileName, CancellationToken ct);
+        Task<string> UploadFileAsync(IFormfile file, string buckectName, string fileName, CancellationToken ct);
         Task DeleteFileAsync(string bucketName,string fileUrl, CancellationToken ct);
     }
 }
