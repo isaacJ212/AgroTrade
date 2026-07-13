487893a1e564e0ac7ae323918e2e5a1d35559c0a
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-13 13:19:22 -0600

    feat(backend/ImpactoSocial): se procedio a hacer la migracioes hacia la BB

--- Backend/src/Meseta_Verde.Infrastructure/Persistence/Migrations/20260713174944_AddImpactoSocialNavigationAndFechaRegistro.Designer.cs
+++ Backend/src/Meseta_Verde.Infrastructure/Persistence/Migrations/20260713174944_AddImpactoSocialNavigationAndFechaRegistro.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Meseta_Verde.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Meseta_Verde.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(MesetaVerdeDbContext))]
-    partial class MesetaVerdeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260713174944_AddImpactoSocialNavigationAndFechaRegistro")]
+    partial class AddImpactoSocialNavigationAndFechaRegistro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
