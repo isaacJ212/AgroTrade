c84127b8e0bee86b652e9849cbc3234c40d0a15e
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-16 22:54:36 -0600

    feat(backend/Conversaciones): creacion del los dto para lo luego proceder con los commands y queries

---
+++ Backend/src/Meseta_Verde.Application/Common/DTOs/ConversacionesDtos/SendMessageDto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Meseta_Verde.Application.Common.DTOs.ConversacionesDtos
+{
+    public class SendMessageDto
+    {
+        [Required(ErrorMessage = "El ID de la conversación es obligatorio.")]
+        public int IdConversacion { get; set; }
+
+        [Required(ErrorMessage = "El ID del emisor es obligatorio.")]
+        public int IdUsuarioEmisor { get; set; }
+
+        [Required(ErrorMessage = "El contenido del mensaje no puede estar vacío.")]
+        [MaxLength(1000, ErrorMessage = "El mensaje es demasiado largo.")]
+        public string Contenido { get; set; } = string.Empty;
+    }
+}
