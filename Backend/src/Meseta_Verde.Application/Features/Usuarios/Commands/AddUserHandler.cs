18aded2c67d21856d79f0a35721a5a1fcb142673
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-24 23:54:23 -0600

    fix(backend): correcion en el nombre de Succes a Success

--- Backend/src/Meseta_Verde.Application/Features/Usuarios/Commands/AddUserHandler.cs
+++ Backend/src/Meseta_Verde.Application/Features/Usuarios/Commands/AddUserHandler.cs
@@ -64,7 +64,7 @@ namespace Meseta_Verde.Application.Features.Usuarios.Commands
                     FechaRegistro = user.FechaRegistro
                 };
 
-                return Result<UserDto>.Succes(201, mapped, "Usuario Registrado. Se envió un código de verificación a tu correo.", true);
+                return Result<UserDto>.Success(201, mapped, "Usuario Registrado. Se envió un código de verificación a tu correo.", true);
 
             }
 
