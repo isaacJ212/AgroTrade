7a62ef2d1370f018b4c739b5a1810b7208e4ad6b
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-25 00:05:06 -0600

    chore(backend): Habilitar Swagger en produccion

--- Backend/src/Meseta_Verde/Program.cs
+++ Backend/src/Meseta_Verde/Program.cs
@@ -99,11 +99,13 @@ namespace Meseta_Verde
             var app = builder.Build();
 
           
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // Habilitar Swagger siempre (tanto en Local como en Producción en Render)
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {       
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meseta Verde API v1");
+                c.RoutePrefix = "swagger"; 
+            });
 
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
