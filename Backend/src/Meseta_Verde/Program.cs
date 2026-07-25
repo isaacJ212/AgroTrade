1e836acdad5f4aa09c8b638488a57b505e3e9496
isaacJ212 <isaacejimenez2006@gmail.com>
2026-07-24 23:33:55 -0600

    feat(backend): agregar archivos de doccker para el ddepligue y dokerizar el backend

--- Backend/src/Meseta_Verde/Program.cs
+++ Backend/src/Meseta_Verde/Program.cs
@@ -115,6 +115,25 @@ namespace Meseta_Verde
 
             app.MapControllers();
 
+
+            // Aplicar migraciones de EF Core automáticamente al iniciar el contenedor
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var dbContext = services.GetRequiredService<MesetaVerdeDbContext>();
+                    dbContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Ocurrió un error al aplicar las migraciones en PostgreSQL.");
+                }
+            }
+
+
+
             app.Run();
 
         } } }
