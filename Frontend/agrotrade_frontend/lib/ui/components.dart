78790f6196a89f167b33c12aecb5b053cc71a66c
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-28 23:24:14 -0600

    feat(frontend/Notificaciones):: se crea nuevos widgets globales y se exporta en components

--- Frontend/agrotrade_frontend/lib/ui/components.dart
+++ Frontend/agrotrade_frontend/lib/ui/components.dart
@@ -18,3 +18,4 @@ export 'widgets/accent_info_card.dart';
 export 'widgets/message_bubble.dart';
 export 'widgets/chat_input_field.dart';
 export 'widgets/profile_menu_item.dart';
+export 'widgets/notification_tile.dart';
