7311548fb881402532e40b5b6b6423c596225a29
Fabian Arteaga <fabianarte121@gmail.com>
2026-08-28 18:27:09 -0600

    fix(frontend/splash): cambio en el icono de la aplicacion y modificacion en el splash de la app

--- Frontend/agrotrade_frontend/ios/Flutter/flutter_export_environment.sh
+++ Frontend/agrotrade_frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\DELL LATITUDE 5400\UNI\FLUTTER\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\UNI\Hackaton Meseta Verde\Meseta-Verde\Frontend\agrotrade_frontend"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\UNI\Hackaton Meseta Verde\Meseta-Verde\Frontend\agrotrade_frontend\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Hackathon\Frontend\agrotrade_frontend"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Hackathon\Frontend\agrotrade_frontend\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
