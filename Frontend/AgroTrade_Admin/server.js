d35bf1eaf25a70f4d8121463466df571119780c6
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-28 21:28:45 -0600

    chore(fronted/Administracion): se agregra morgan logger y SPA routing

--- Frontend/AgroTrade_Admin/server.js
+++ Frontend/AgroTrade_Admin/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use(morgan(':date[iso] | :remote-addr | :method :url | Status: :status | :response-time ms'));
+
+
 app.use(express.static(path.join(__dirname, '')));
 
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use((req, res, next) => {
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } else {
+    res.status(404).send('Recurso no encontrado');
+  }
 });
 
 app.listen(PORT, () => {
