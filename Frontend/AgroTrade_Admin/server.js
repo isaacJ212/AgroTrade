f586937f883e973df8e9c58e3fa16bea6d10dc08
isaacJ212 <isaacejimenez2006@gmail.com>
2026-09-03 04:13:53 -0600

    feat(frontend/conexion): agregar soporte para variables de entorno con dotenv

--- Frontend/AgroTrade_Admin/server.js
+++ Frontend/AgroTrade_Admin/server.js
@@ -5,9 +5,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config();
 
 app.use(morgan(':date[iso] | :remote-addr | :method :url | Status: :status | :response-time ms'));
 
+// Ruta dinámica para variables de entorno
+app.get('/env-config.js', (req, res) => {
+  res.type('application/javascript');
+  res.send(`
+    window.ENV = {
+      API_BASE_URL: "${process.env.API_BASE_URL || 'http://localhost:5080/api'}"
+    };
+  `);
+});
 
 app.use(express.static(path.join(__dirname, '')));
 
